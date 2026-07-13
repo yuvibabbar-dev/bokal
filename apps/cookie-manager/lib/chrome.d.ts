b17a6a032a3f64d996974f20f944a5dc18faacde
yuvibabbar-dev <ljamestto007@gmail.com>
2026-07-13 01:59:02 -0700

    refactor(cookie-manager): document + relocate chrome.permissions type shim

--- apps/cookie-manager/lib/chrome.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace chrome.permissions {
-  interface PermissionsAddedEvent {
-    removeListener(callback: (permissions: Permissions) => void): void;
-  }
-
-  interface PermissionsRemovedEvent {
-    removeListener(callback: (permissions: Permissions) => void): void;
-  }
-}
